15d3c8140a04e70f985ba85838724f250113738a
Dongwoo Lee <dongwoolee@MacBook-Pro-2.local>
2025-07-24 10:30:46 +0900

    Disable ESLint and TypeScript checks during build

--- next.config.js
+++ next.config.js
@@ -1,20 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['eaeulxmxiwnxopnjrvun.supabase.co'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'eaeulxmxiwnxopnjrvun.supabase.co',
-        port: '',
-        pathname: '/storage/v1/object/public/**',
-      },
-    ],
-  },
-}
-
-module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-}; 
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+module.exports = nextConfig; 
